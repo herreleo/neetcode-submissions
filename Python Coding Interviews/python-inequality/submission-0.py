28e496d2cf1a4f369b750e612f7922255b98543b
herreleo <herreraleonel297@gmail.com>
2026-07-26 18:41:33 -0400

    Add: python-inequality - submission-0

---
+++ Python Coding Interviews/python-inequality/submission-0.py
@@ -0,0 +1,20 @@
+from typing import List
+
+
+def is_arr_valid(names: List[str], max_length: int) -> bool:
+    list_len = len(names)
+
+    if 0 < list_len <= max_length:
+        return True
+    
+    else: 
+        return False
+
+
+
+# do not modify below this line
+print(is_arr_valid(["Alice", "Bob", "Charlie"], 3))
+print(is_arr_valid(["Alice", "Bob", "Charlie"], 2))
+print(is_arr_valid(["Alice", "Bob", "Charlie"], 0))
+print(is_arr_valid(["Alice", "Bob", "Charlie"], 1))
+print(is_arr_valid(["Alice", "Bob", "Charlie"], 4))
